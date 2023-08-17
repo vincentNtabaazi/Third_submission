59ea5a7e2f706fe730689de6b493e014cb0a1c86
vincentNtabaazi <vincentntabaazi02@gmail.com>
2023-08-17 10:57:18 +0300

    wrote code required when hosting an application and the necessary settings.

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn TodoApp.wsgi --log-file--
+web: python manage.py collectstatic --noinput
--- TodoApp/settings.py
+++ TodoApp/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = '/home/username/projects/site/assets/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
