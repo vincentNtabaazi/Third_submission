1bb2108182ca6218c90efced932ed8fdcd9a5dfc
vincentNtabaazi <vincentntabaazi02@gmail.com>
2023-08-17 13:04:33 +0300

    deployment

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-release: python manage.py collectstatic
 web: gunicorn TodoApp.wsgi
--- TodoApp/settings.py
+++ TodoApp/settings.py
@@ -27,6 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-d3y6uk*l$c=$v$sx+oa#(8hry_-f+21%af3cuvl6ml%m96(6l#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = False
 # if (len(sys.argv) >= 2 and sys.argv[1] == 'runserver'):
 #     DEBUG = True
